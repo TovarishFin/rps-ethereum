7f24ce2b3fcb68b494ebfbf12d974b8ec85ca017
TovarishFin <tovarishfin@gmail.com>
2018-12-01 23:52:44 +0100

    added bank vuex store actions (contract setters)

--- src/store/bank/actions.js
+++ src/store/bank/actions.js
@@ -1,5 +1,9 @@
 import Bank from '@/../contractsBuild/Bank'
 
+//
+// start setup related
+//
+
 export const setupBank = async ({ commit, getters }) => {
   const { web3, networkId } = getters
   const bnk = new web3.eth.Contract(Bank.abi, Bank.networks[networkId].address)
@@ -17,6 +21,14 @@ export const setupBankWs = async ({ commit, getters }) => {
 
   commit('setBankWs', bnk)
 }
+
+//
+// end setup related
+//
+
+//
+// start contract getters
+//
 
 export const getWethAddress = async ({ commit, getters }) => {
   const { bnk } = getters
@@ -51,3 +63,52 @@ export const getCoinbaseAllocatedTokens = async (
     .call()
   commit('setCoinbaseAllocatedTokens', { tokenAddress, amount })
 }
+
+//
+// end contract getters
+//
+
+//
+// start contract setters
+//
+
+export const depositEtherFor = async ({ getters }, { userAddress, value }) => {
+  const { bnk, coinbase } = getters
+  await bnk.methods.depositEtherFor(userAddress).send({
+    from: coinbase,
+    value
+  })
+}
+
+export const depositEther = async ({ getters }, value) => {
+  const { bnk, coinbase } = getters
+  await bnk.methods.depositEther().send({
+    from: coinbase,
+    value
+  })
+}
+
+export const withdrawEther = async ({ getters }, value) => {
+  const { bnk, coinbase } = getters
+  await bnk.methods.withdrawEther(value).send({
+    from: coinbase
+  })
+}
+
+export const depositTokens = async ({ getters }, { tokenAddress, value }) => {
+  const { bnk, coinbase } = getters
+  await bnk.methods.depositTokens(tokenAddress, value).send({
+    from: coinbase
+  })
+}
+
+export const withdrawTokens = async ({ getters }, { tokenAddress, value }) => {
+  const { bnk, coinbase } = getters
+  await bnk.methods.withdrawTokens(tokenAddress, value).send({
+    from: coinbase
+  })
+}
+
+//
+// end contract setters
+//
